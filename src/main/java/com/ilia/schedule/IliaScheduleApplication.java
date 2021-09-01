701d8239c40545d53b98b787c7026df2567855aa
Alejandro Planos Gonzalez <aplanos@gmail.com>
2021-09-01 18:41:42 -0300

    initial commit
    Spring Boot generated at https://start.spring.io/ with starter web and lombok dependencies.

---
+++ src/main/java/com/ilia/schedule/IliaScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.ilia.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IliaScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IliaScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilia/schedule/IliaScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilia.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IliaScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
