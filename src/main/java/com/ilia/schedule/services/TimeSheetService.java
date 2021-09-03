dd713e5d4120f2cc614cd1bb0da1941ba80db288
Alejandro Planos Gonzalez <aplanos@gmail.com>
2021-09-03 11:59:39 -0300

    docs: add java docs to TimeSheetService

--- src/main/java/com/ilia/schedule/services/TimeSheetService.java
+++ src/main/java/com/ilia/schedule/services/TimeSheetService.java
@@ -1,7 +1,22 @@
 package com.ilia.schedule.services;
 
+import com.ilia.schedule.repositories.models.TimeSheet;
 import com.ilia.schedule.services.dto.TimeSheetDto;
+import com.ilia.schedule.services.exceptions.CheckedTimeExistException;
+import com.ilia.schedule.services.exceptions.CheckedTimeInvalidException;
 
 public interface TimeSheetService {
+    /**
+     * Returns a TimeSheetDto object when is successfully created in repository.
+     * The checkedDateTime field cannot be null.
+     *
+     * @param timeSheetDto a TimeSheetDto instance object
+     * @return a new TimeSheetDto instance object
+     * @throws CheckedTimeInvalidException when required checkedDateTime is null
+     * @throws IllegalArgumentException when some rule is broken, example: more than four entries by day
+     * @throws CheckedTimeExistException when the provide checkedDateTime already is store in database
+     * @see TimeSheetDto
+     * @see TimeSheet
+     */
     TimeSheetDto saveCheckedDateTime(TimeSheetDto timeSheetDto);
 }
