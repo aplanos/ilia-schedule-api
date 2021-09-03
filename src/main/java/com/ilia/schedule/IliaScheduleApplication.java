cd409f719fb5aaac38163cbf2119189183a2f7b0
Alejandro Planos Gonzalez <aplanos@gmail.com>
2021-09-03 01:47:49 -0300

    feat: add CORS support and open api servers property

--- src/main/java/com/ilia/schedule/IliaScheduleApplication.java
+++ src/main/java/com/ilia/schedule/IliaScheduleApplication.java
@@ -3,6 +3,9 @@ package com.ilia.schedule;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAdminServer
 @SpringBootApplication
@@ -12,4 +15,14 @@ public class IliaScheduleApplication {
 		SpringApplication.run(IliaScheduleApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/**")
+						.allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
